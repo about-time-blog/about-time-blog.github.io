4e9cfcf6eba64edcfa445d7101e2fc6ded4515a1
Mikkel Riise Lund <mrl@cvation.com>
2019-09-28 20:04:16 +0200

    Tries to improve loadtimes with critical css.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,4 @@
-// Unhide the content and jump to the right place on the page at the same time
-function restoreAndSkipContent() {
-    var topPaddingInPixels = $('body').css('padding-top');
-    var topPadding = parseInt(topPaddingInPixels);
-
-    var hidden = $('.skip-me');
-    var hiddenHeight = hidden.outerHeight();
-
-    var x = hiddenHeight + topPadding;
-
-    hidden.addClass('unhide');
-    window.scroll(0, x);
-};
-
-
 $(function() {
-    // restoreAndSkipContent();
-
     $('[data-spy="scroll"]').each(function () {
         $(this).scrollspy('refresh');
     })
@@ -23,5 +6,5 @@ $(function() {
     $('[data-toggle="tooltip"]').tooltip();
 
     // Animations initialization
-    new WOW().init();    
+    new WOW().init();
 });
